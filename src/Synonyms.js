f8dd519b7897902deabbeb295fd8f04c13f8179e
AudreyLovelace <yueyueaudrey2021@gmail.com>
2022-01-13 11:46:44 -0400

    Update Synonyms.js

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -2,7 +2,7 @@ export default function Synonyms(props) {
   if (props.words[0] !== undefined) {
     return (
       <ul>
-        <h4>synonyms:</h4>
+        <h4>Similar:</h4>
         {props.words.map((element, index) => {
           return (
             <li key={index}>
